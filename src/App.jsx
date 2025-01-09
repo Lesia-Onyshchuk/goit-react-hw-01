3c7b7c124c0262ac4c4d05723a4a0deff2b8cae5
lesia_onyshchuk <lkane443@gmail.com>
2025-01-09 22:18:29 +0200

    transactions

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import userData from "./userData.json";
 import "./App.css";
 import FriendList from "./components/FriendList/FriendList";
 import friends from "./friends.json";
+import transactions from "./transactions.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 const App = () => {
   return (
@@ -15,6 +17,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
